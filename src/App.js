76b6d2362655f2dce2a513bbeb0d2aa6d235e82c
antoniab79 <antonia@webleviathan.com>
2024-09-30 00:27:32 +0100

    updated spaces in footer

--- src/App.js
+++ src/App.js
@@ -10,10 +10,9 @@ export default function App() {
       <main>
         <Dictionary />
       </main>
-      <footer>This project was coded by Antonia, it's open-sourced on
-        <a href="https://github.com/antoniab79/dictonary-project">Github</a>, and hosted on 
+      <footer>This project was coded by Antonia, it's open-sourced on { " " }
+        <a href="https://github.com/antoniab79/dictonary-project">Github</a>, and hosted on { " " }
         <a href="https://react-word-lookup.netlify.app/">Netlify</a>
-
       </footer>
     </div>
     </div>
