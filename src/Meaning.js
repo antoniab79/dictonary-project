96c7eb88c81c1bfe8956522f7a296aac29b1c172
antoniab79 <antonia@webleviathan.com>
2024-10-05 14:15:10 +0100

    added synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
     return (
@@ -8,13 +9,14 @@ export default function Meaning(props) {
         {
             return (
                 <div key={index}>
-                    <p>{definition.definition}
-            <br />
-            <em>{definition.example}</em>
-            </p>
-            </div>
-        );
-    })}
+                        <strong>Definition:</strong> {definition.definition}
+                        <br />
+                        <strong>Example:</strong> {definition.example}
+                        <br />
+                        <Synonyms synonyms={definition.synonyms} />
+                </div>
+            );
+        })}
     </div>    
     );
 }
